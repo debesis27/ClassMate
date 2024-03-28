29254219fdf608263003fdd055cf375ff7065ee3
debesis27 <shivam27.maske@gmail.com>
2024-03-28 15:00:26 +0530

    Refactor add_new_course.dart and home_screen_teacher.dart

--- lib/Screens/Teacher/add_new_course.dart
+++ lib/Screens/Teacher/add_new_course.dart
@@ -14,56 +14,144 @@ class _AddNewCourse extends State<AddNewCourse> {
   String academicYear = '';
 
   void saveInputs() {
-    // Save the inputs from the text fields
-    // You can use the values of courseCode, courseTitle, and academicYear here
+    Course newCourse = Course(
+      code: courseCode,
+      title: courseTitle,
+      academicYear: academicYear,
+      instructor: 'Kaushik Mondal',
+      image: 'assets/mathematics.jpg',
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue, // Set the background color to blue
         title: const Text('Add New Course'),
       ),
       body: Center(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: <Widget>[
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Course Code',
-              ),
-              onChanged: (value) {
-                setState(() {
-                  courseCode = value;
-                });
-              },
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Course Title',
-              ),
-              onChanged: (value) {
-                setState(() {
-                  courseTitle = value;
-                });
-              },
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Academic Year',
-              ),
-              onChanged: (value) {
-                setState(() {
-                  academicYear = value;
-                });
-              },
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 20.0,bottom: 10.0),
+                    child: const Text(
+                      'Course Code',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10.0),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        courseCode = value;
+                      });
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: const Text(
+                      'Course Title',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10.0),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        courseTitle = value;
+                      });
+                    },
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: const Text(
+                      'Academic Year',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10.0),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        academicYear = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
             ElevatedButton(
-              onPressed: saveInputs,
-              child: const Text('Save'),
-            ),
+                    onPressed: saveInputs,
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      minimumSize: const Size(double.infinity, 48.0),
+                    ),
+                    child: const Text('Save'),
+                  ),
           ],
         ),
-      ),
+      )),
     );
   }
 }
--- lib/Screens/Teacher/home_screen_teacher.dart
+++ lib/Screens/Teacher/home_screen_teacher.dart
@@ -74,7 +74,7 @@ class _TeacherHomePage extends State<TeacherHomePage> {
         },
         backgroundColor: Colors.blue,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
+          borderRadius: BorderRadius.circular(25.0),
         ),
         child: const Icon(Icons.add),
       ),
